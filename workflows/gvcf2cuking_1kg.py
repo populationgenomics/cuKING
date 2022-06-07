8a6e2ca9b53053b49c71830be5a97c03df576410
Leonhard Gruenschloss <leonhard@gruenschloss.org>
2022-06-07 09:38:46 +0000

    Fix paths

--- workflows/gvcf2cuking_1kg.py
+++ workflows/gvcf2cuking_1kg.py
@@ -6,6 +6,7 @@ from cpg_utils.hail_batch import get_config, remote_tmpdir, output_path
 
 DOCKER_IMAGE = 'australia-southeast1-docker.pkg.dev/cpg-common/images/cuking:d4877256d0b6e3a46465390e249106832e2c90af'
 
+
 def main():
     config = get_config()
 
@@ -16,13 +17,17 @@ def main():
 
     batch = hb.Batch(backend=service_backend)
 
-    for gvcf_path in open('1kg_gvcfs.txt'):
-        job = batch.new_job(os.path.basename(gvcf_path))
-        job.image(DOCKER_IMAGE)
-        job.memory('lowmem')
-        job.command('export GCS_OAUTH_TOKEN=$(gcloud auth application-default print-access-token)')
-        cuking_path = output_path(gvcf_path.replace('.g.vcf.gz', '.cuking'))
-        job.command(f'gvcf2cuking --input={gvcf_path} --output={cuking_path}')
+    with open('1kg_gvcfs.txt') as f:
+        for gvcf_path in f.readlines():
+            basename = os.path.basename(gvcf_path)
+            job = batch.new_job(basename)
+            job.image(DOCKER_IMAGE)
+            job.memory('lowmem')
+            job.command(
+                'export GCS_OAUTH_TOKEN=$(gcloud auth application-default print-access-token)'
+            )
+            cuking_path = output_path(basename.replace('.g.vcf.gz', '.cuking'))
+            job.command(f'gvcf2cuking --input={gvcf_path} --output={cuking_path}')
 
     batch.run(wait=False)
 
