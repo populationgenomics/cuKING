e4ee3981086fc51364375d899208f1a85643d854
Leonhard Gruenschloss <leonhard@gruenschloss.org>
2022-06-07 09:25:52 +0000

    Add 1kg conversion workflow

---
+++ workflows/gvcf2cuking_1kg.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+import hailtop.batch as hb
+from cpg_utils.hail_batch import get_config, remote_tmpdir, output_path
+
+DOCKER_IMAGE = 'australia-southeast1-docker.pkg.dev/cpg-common/images/cuking:d4877256d0b6e3a46465390e249106832e2c90af'
+
+def main():
+    config = get_config()
+
+    service_backend = hb.ServiceBackend(
+        billing_project=config['hail']['billing_project'],
+        remote_tmpdir=remote_tmpdir(),
+    )
+
+    batch = hb.Batch(backend=service_backend)
+
+    for gvcf_path in open('1kg_gvcfs.txt'):
+        job = batch.new_job(os.path.basename(gvcf_path))
+        job.image(DOCKER_IMAGE)
+        job.memory('lowmem')
+        job.command('export GCS_OAUTH_TOKEN=$(gcloud auth application-default print-access-token)')
+        cuking_path = output_path(gvcf_path.replace('.g.vcf.gz', '.cuking'))
+        job.command(f'gvcf2cuking --input={gvcf_path} --output={cuking_path}')
+
+    batch.run(wait=False)
+
+
+if __name__ == '__main__':
+    main()
