e8fe804ecdfc7d0f9ac6adba43704813efe3c62f
Leonhard Gruenschloss <leonhard@gruenschloss.org>
2022-06-21 07:29:27 +0000

    Avoid copy when reading from GCS

--- gcs_client.cc
+++ gcs_client.cc
@@ -67,13 +67,9 @@ class GcsClientImpl : public GcsClient {
         return absl::NotFoundError("Couldn't find content-length header");
       }
 
-      std::ostringstream os;
-      os << reader.rdbuf();
-      if (reader.bad()) {
-        return absl::InvalidArgumentError(absl::StrCat(
-            "Failed to read blob ", url, ": ", reader.status().message()));
-      }
-      return os.str();
+      std::string result(*content_length, 0);
+      reader.read(result.data(), *content_length);
+      return std::move(result);
     } catch (const std::exception& e) {
       // Unfortunately the googe-cloud-storage library throws exceptions.
       return absl::InternalError(
