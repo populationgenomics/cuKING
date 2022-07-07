a8f459d411683c76de9b9aef4a358e25352a6fb4
Leonhard Gruenschloss <leonhard.gruenschloss@populationgenomics.org.au>
2022-07-07 13:55:54 +1000

    Add LD-pruning

--- scripts/relatedness_comparison.py
+++ scripts/relatedness_comparison.py
@@ -58,42 +58,77 @@ def main():
     mt = annotate_adj(mt)
     mt = mt.filter_entries(mt.adj)
 
-    # Store GT-only table before running relatedness methods.
+    # Store GT-only table.
     mt = mt.select_entries(mt.GT)
     mt = write_if_not_exists(
         mt, 'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites.mt'
+    )
+
+    # LD-prune.
+    mt = mt.unfilter_entries()
+    pruned_ht = hl.ld_prune(mt.GT, r2=0.1)
+    mt = mt.filter_rows(hl.is_defined(pruned_ht[mt.row_key]))
+    mt = write_if_not_exists(
+        mt, 'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pruned.mt'
     )
 
     # Compute KING.
     king = hl.king(mt.GT)
     king = write_if_not_exists(
         king,
-        'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_king.mt',
+        'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pruned_king.mt',
     )
 
     # Compute PCA for PC-Relate.
-    _, scores, _ = hl.hwe_normalized_pca(mt.GT, k=10, compute_loadings=False)
-
+    _, scores, loadings = hl.hwe_normalized_pca(mt.GT, k=10, compute_loadings=True)
     scores = write_if_not_exists(
         scores,
-        'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pca_scores.ht',
+        'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pruned_pca_scores.ht',
+    )
+    loadings = write_if_not_exists(
+        loadings,
+        'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pruned_pca_loadings.ht',
     )
 
-    # Compute PC-Relate.
-    for min_individual_maf in (0.001, 0.01, 0.05):
-        # Typically we'd pass min_kinship=0.05 here, but in order to compare with KING
-        # results, we don't filter.
-        pc_relate = hl.pc_relate(
-            mt.GT,
-            min_individual_maf=min_individual_maf,
-            scores_expr=scores[mt.col_key].scores,
-            block_size=4096,
-            statistics="all",
-        )
-        pc_relate = write_if_not_exists(
-            pc_relate,
-            f'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pc_relate_{min_individual_maf}_based_on_scores.ht',
-        )
+    def pc_relate(mt, scores_expr, file_name_suffix):
+        """Runs PC-Relate for a few different min_individual_maf values."""
+        for min_individual_maf in (0.001, 0.01, 0.05):
+            # Typically we'd pass min_kinship=0.05 here, but in order to compare with KING
+            # results, we don't filter.
+            ht = hl.pc_relate(
+                mt.GT,
+                min_individual_maf=min_individual_maf,
+                scores_expr=scores_expr,
+                block_size=4096,
+                statistics="all",
+            )
+            write_if_not_exists(
+                ht,
+                f'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pruned_pc_relate_{min_individual_maf}_{file_name_suffix}.ht',
+            )
+
+    # Compute PC-Relate for the new scores.
+    pc_relate(mt, scores[mt.col_key].scores, 'new_scores')
+
+    # The previous scores only contain UKB samples, therefore subset samples first.
+    previous_scores = hl.read_table('gs://sites-for-relatedness-transfer-au-tmp/pruned.ukb_pca_scores.ht')
+    mt = mt.filter_cols(hl.is_defined(previous_scores[mt.s]))
+    mt = write_if_not_exists(
+        mt, 'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pruned_ukb_only.mt'
+    )
+    pc_relate(mt, previous_scores[mt.col_key].scores, 'previous_scores')
+
+    # Compute new scores only for the UKB sample subset and recompute PC-Relate.
+    _, scores, loadings = hl.hwe_normalized_pca(mt.GT, k=10, compute_loadings=True)
+    scores = write_if_not_exists(
+        scores,
+        'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pruned_pca_scores_ukb_only.ht',
+    )
+    loadings = write_if_not_exists(
+        loadings,
+        'gs://sites-for-relatedness-transfer-au-tmp/gnomad_v4.0_test_ukb_sites_pruned_pca_loadings_ukb_only.ht',
+    )
+    pc_relate(mt, scores[mt.col_key].scores, 'new_scores_ukb_only')
 
 
 if __name__ == '__main__':
